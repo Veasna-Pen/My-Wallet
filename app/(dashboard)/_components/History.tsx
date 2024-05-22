defeb0196dd0190310967c68e7dd858f2ca73ce1
veasna3d <veasna3d@gmail.com>
2024-05-22 21:24:00 +0700

    History Chart Component

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { GetFormatterForCurrency } from "@/lib/helper";
 import { Period, Timeframe } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import HistoryPeriodSelector from "./HistoryPeriodSelector";
 import { useQuery } from "@tanstack/react-query";
 import SkeletonWrapper from "@/components/SkeletonWrapper";
@@ -19,6 +19,8 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import { cn } from "@/lib/utils";
+import CountUp from "react-countup";
 
 const History = ({ userSettings }: { userSettings: UserSettings }) => {
   const [timeframe, setTimeframe] = useState<Timeframe>("month");
@@ -159,7 +161,12 @@ const History = ({ userSettings }: { userSettings: UserSettings }) => {
                     className="cursor-pointer"
                   />
 
-                  
+                  <Tooltip
+                    cursor={{ opacity: 0.1 }}
+                    content={(props) => (
+                      <CustomTooltip formatter={formatter} {...props} />
+                    )}
+                  />
                 </BarChart>
               </ResponsiveContainer>
             )}
@@ -179,3 +186,76 @@ const History = ({ userSettings }: { userSettings: UserSettings }) => {
 };
 
 export default History;
+
+const CustomTooltip = ({ active, payload, formatter }: any) => {
+  if (!active || !payload || payload.length === 0) return null;
+
+  const data = payload[0].payload;
+  const { expense, income } = data;
+
+  return (
+    <div className="min-w-[300px]  rounded border bg-background p-4">
+      <TooltipRow
+        formatter={formatter}
+        label="Expense"
+        value={expense}
+        bgColor="bg-red-500"
+        textColor="text-red-500"
+      />
+      <TooltipRow
+        formatter={formatter}
+        label="Income"
+        value={income}
+        bgColor="bg-emerald-500"
+        textColor="text-emerald-500"
+      />
+      <TooltipRow
+        formatter={formatter}
+        label="Balance"
+        value={income - expense}
+        bgColor="bg-gray-100"
+        textColor="text-foreground"
+      />
+    </div>
+  );
+};
+
+const TooltipRow = ({
+  label,
+  value,
+  bgColor,
+  textColor,
+  formatter,
+}: {
+  label: string;
+  textColor: string;
+  bgColor: string;
+  value: number;
+  formatter: Intl.NumberFormat;
+}) => {
+  const formattingFn = useCallback(
+    (value: number) => {
+      return formatter.format(value);
+    },
+    [formatter]
+  );
+
+  return (
+    <div className="flex items-center gap-2">
+      <div className={cn("h-4 w-4 rounded-full", bgColor)} />
+      <div className="flex w-full justify-between">
+        <p className="text-sm text-muted-foreground">{label}</p>
+        <div className={cn("text-sm font-bold", textColor)}>
+          <CountUp
+            duration={0.5}
+            preserveValue
+            end={value}
+            decimals={0}
+            formattingFn={formattingFn}
+            className="text-sm"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
